648edc7989a9c9ce7a9451de30c71ecfbc94a692
brit13-meet <brit13@meet.mit.edu>
2016-02-23 19:51:58 +0200

    pizza class

---
+++ pizza.py
@@ -0,0 +1,23 @@
+class pizza(object):
+	def __init__(self,x,y,width, height,health):
+		self.x = x
+		self.y= y
+		self.width = width
+		self.height= height
+		self.health= health
+		self.disappear= false
+	def change(self.health):
+		if (ballhit== true and self.disappear== false):
+			changetocheese
+			self.disappear= true
+		elif( ballhit == true and self.disappear == true):
+			pizza.disappear
+		
+		
+
+
+		
+		
+
+
+	
